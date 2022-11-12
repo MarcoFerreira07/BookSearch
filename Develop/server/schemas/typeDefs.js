8fe7f4bbb18a251d0b5b8e4e3e8922bd66ecd6f9
Marco Ferreira <marcoasf.com@gmail.com>
2022-11-11 21:58:51 -0600

    completed querey mutation user book and auth type as per readme rules

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,27 +1,33 @@
 const { gql } = require("apollo-server-express");
 const typeDefs = gql`
   type Book {
-    _id: ID!
+    bookId: String
     authors: [String]!
-    description: String!
-    bookId: String!
+    title: String
+    description: String
     image: String
     link: String
-    title: String
   }
   type User {
-    _id: ID!
-    email: String!
-    password: String!
-    savedBooks:
+    _id: ID
+    username: String
+    email: String
+    password: String
+    bookCount: Int
+    savedBooks: [Book]!
+  }
+  type Auth {
+    token: ID!
+    user: User
   }
   type Query {
-    tech: [Tech]
-    matchups(_id: String): [Matchup]
+  me: User
   }
   type Mutation {
-    createMatchup(tech1: String!, tech2: String!): Matchup
-    createVote(_id: String!, techNum: Int!): Matchup
-  }
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(userId: ID!, bookId: String!, authors: [String]!, title:String!, description: String!, image:String!, link: String): User
+    removeBook(userId: ID!, bookId: String!): User
 `;
 module.exports = typeDefs;
+
