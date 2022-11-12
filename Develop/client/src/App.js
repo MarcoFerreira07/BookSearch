331b84e0d6064057a66af3b714693d672c78304d
Marco Ferreira <marcoasf.com@gmail.com>
2022-11-11 22:45:56 -0600

    compleeted app.js added all apollo reqs..

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -3,9 +3,39 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers}) =>{
+  const token = localStroage.getItem("id_token");
+
+  return{
+    headers:{
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+
+});
+
+
 
 function App() {
   return (
+    <ApolloProvider client = {client}>
     <Router>
       <>
         <Navbar />
@@ -25,6 +55,7 @@ function App() {
         </Routes>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -26,7 +26,7 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(userId: ID!, authors: [String]!, description: String!, title:String!, bookId: String!, image:String!, link: String): User
+    saveBook(userId: ID!, authors: [String]!, description: String!, title:String!, bookId: String!, image:String!, link: String!): User
     removeBook(userId: ID!, bookId: String!): User
 `;
 module.exports = typeDefs;
